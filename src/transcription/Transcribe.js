3a210286a98dc628854604fee5e25e3fa10374fc
AkidaSoto <60154963+AkidaSoto@users.noreply.github.com>
2020-07-19 05:30:08 -0400

    Transcript Part 2

--- src/transcription/Transcribe.js
+++ src/transcription/Transcribe.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import natural from "natural"
-
+//import message from "message_1.json"
 //var wordnet = new natural.WordNet('./WordNet/2.1/dict');
 //mport * as firebase from "firebase";
 //import fbConfig from "../firebase-config";
@@ -9,7 +9,11 @@ import natural from "natural"
 // firebase.initializeApp(fbConfig);
 // const dataBase = firebase.firestore();
 
-var tokenizer = new natural.WordTokenizer();
+var tokenizer = new natural.TreebankWordTokenizer();
+var Analyzer = natural.SentimentAnalyzer;
+var stemmer = natural.PorterStemmer;
+
+var analyzer = new Analyzer("English", stemmer, "afinn");
 
 const language = "EN"
 const defaultCategory = 'N';
@@ -19,13 +23,108 @@ var lexicon = new natural.Lexicon(language, defaultCategory, defaultCategoryCapi
 var ruleSet = new natural.RuleSet('EN');
 var tagger = new natural.BrillPOSTagger(lexicon, ruleSet);
 
+function diff(A) {
+  return A.slice(1).map(function(n, i) { return (n - A[i]) != 0; });
+}
+
+
+function readJsonFile(jsonFile) {
+  var reader = new FileReader();
+  reader.addEventListener('load', (loadEvent) => {
+    try {
+      var json = JSON.parse(loadEvent.target.result);
+      console.log(json);
+      console.log(json.messages)
+
+      var participants = json.participants.map(x => x.name);
+      console.log(participants);
+      
+      var par_index = json.messages.map(x => participants.indexOf(x.sender_name));
+      console.log(par_index);
+
+      var par_index_diff = diff(par_index)
+      par_index_diff.unshift(true)
+      par_index_diff.push(true)
+      console.log(par_index_diff);
+
+      const indices = par_index_diff.reduce(
+        (out, bool, index) => bool ? out.concat(index) : out, 
+        []
+      )
+
+      console.log(indices)
+      var concat_messages = [];
+      var block_nodes = [];
+      var sentiment = [];
+      var i;
+      var j;
+      for (i = 0; i < indices.length-1; i++){
+        console.log(indices[i])
+
+        for (j = indices[i]; j < [indices[i+1]]; j++) {
+
+          if (("content" in json.messages[j]) ){
+          json.messages[j].content = json.messages[j].content.replace(/‘|_/g,"'") 
+          json.messages[j].content = json.messages[j].content.replace(/â|_/g,"'") 
+          }
+
+          if (j == indices[i]) {
+            if (("content" in json.messages[j]) ){
+            concat_messages[i] = json.messages[j].content;}
+            else {
+            concat_messages[i] = "";
+
+            }
+          } else {
+            if (("content" in json.messages[j]) ){
+            concat_messages[i]= concat_messages[i].concat('.',json.messages[j].content);}
+            else{
+              concat_messages[i]= concat_messages[i].concat('.');
+            }
+          }
+        }
+        console.log(concat_messages[i])
+        block_nodes[i] = tagger.tag(tokenizer.tokenize(concat_messages[i])).taggedWords.filter(x => ['NN','NNP','NNPS','NNS'].indexOf(x.tag) >= 0).map(x => x.token).filter(x => ['I'].indexOf(x) < 0)
+        sentiment[i] = analyzer.getSentiment(tokenizer.tokenize(concat_messages[i]));
+        console.log(block_nodes[i])
+        console.log(sentiment[i])
+        
+      }
+      console.log(concat_messages)
+      console.log(block_nodes.join('.'))
+      console.log(sentiment)
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  reader.readAsText(jsonFile);
+}
+
+
 const Transcribe = () => {
 
+  
     const ParseText = () => {
+
        var TextArea = document.getElementById("TextArea_");
        var Output1 = document.getElementById("Output1");
        var Output2 = document.getElementById("Output2");
        var Output3 = document.getElementById("Output3");
+       var JSON_File = document.getElementById("jsonFile");
+
+       console.log(JSON_File)
+
+       var fileselect = false;
+       if( JSON_File.files.length == 1 ){
+        console.log("files selected");
+        fileselect = true;
+
+        readJsonFile(JSON_File.files[0])
+       }
+
+
+       
+
        console.log(tokenizer.tokenize(TextArea.value));
 
 
@@ -50,6 +149,9 @@ const Transcribe = () => {
             <textarea id="TextArea_">Insert Text Here</textarea>
             <button onClick={ParseText}>Nodulate</button>
 
+            <input id="jsonFile" name="jsonFile" type="file"></input>
+            <button onClick={ParseText}>Nodulate</button>
+            
           <div id="Output1" style={{ padding: "2em" }}></div>
           <div id="Output2" style={{ padding: "2em" }}></div>
           <div id="Output3" style={{ padding: "2em" }}></div>
